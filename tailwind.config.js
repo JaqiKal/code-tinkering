fc1bdbc5e3474b0164e564ea8e2d1f391b5a0de2
Jacqueline Kalmár <jaqika@gmail.com>
2024-10-22 15:44:44 +0200

    style: root index styled & Add tailwind for next proj

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",  // Keep this to watch files inside the 'src' folder
+    "./*.html",              // To watch any HTML files in the root directory
+    "./projects/**/*.html",  // To watch HTML files in the 'projects' folder and subfolders
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
