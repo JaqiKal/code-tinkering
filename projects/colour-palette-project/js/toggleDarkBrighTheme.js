0bdba3b4eb27826811be21278601513202010d27
Jacqueline Kalmár <jaqika@gmail.com>
2024-10-23 15:53:18 +0200

    refactor: Change cdn to local tailwind and add style
    Add javascript to toggle dark/bright theme
    
    Add bg color to quadrants

---
+++ projects/colour-palette-project/js/toggleDarkBrighTheme.js
@@ -0,0 +1,5 @@
+// Toggle between dark and light mode
+const toggleButton = document.getElementById('theme-toggle');
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+});
